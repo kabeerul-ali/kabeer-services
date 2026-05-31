48e2aef662b1002da9603de9f0d4dd3d33345aad
kabeerul-ali <kabeerulali@gmail.com>
2026-05-31 11:19:51 +0530

    change the service detail

--- src/pages/SubServicePage.jsx
+++ src/pages/SubServicePage.jsx
@@ -92,7 +92,7 @@ ${currentUrl} \n\n\n\nThanks!\n\n\n`
 
             <div className="p-4">
 
-              <h2 className="fw-bold mb-3 text-light">
+              <h2 className="fw-bold mb-3 text-primary">
 
                 {subService.name}
 
